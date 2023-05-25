029df7de45d60cfbbe413bc8250cab23fb3a43d7
AntonioRodriguezUFAM <antonio.rodriguez@icom.ufam.edu.br>
2023-05-24 20:31:00 -0400

    added Optic Flow Motion Detection

--- Image_PPM_Processing_MultiThreads_AnyOS.cpp
+++ Image_PPM_Processing_MultiThreads_AnyOS.cpp
@@ -4,6 +4,11 @@ This code should now be compatible with both Windows and Linux operating systems
 It uses preprocessor directives to include the necessary headers and handle system-specific functions
 */
 
+/*
+added the opticFlow function to perform the Optic Flow algorithm for motion detection.
+The function is called after inverting the colors of the image. The main function now includes the execution of the opticFlow function and waits for user input before exiting the program, depending on the operating system
+*/
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -120,6 +125,152 @@ public:
         auto duration = duration_cast<milliseconds>(end - start);
         cout << "---------------  Run Parallel: Invert Color Time:" << duration.count() << "ms" << endl;
     }
+
+
+   // Optic Flow - Motion Detection Secuential 
+    void opticFlow() {
+        auto start = high_resolution_clock::now(); // Start Timer
+
+        // Define parameters for optic flow calculation
+        int windowSize = 5; // Size of the neighborhood window
+        double threshold = 10.0; // Threshold for motion detection
+
+        // Create a new image to store the motion vectors
+        Image motionVectors(width, height, 255);
+
+        // Iterate over the image pixels
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                // Calculate motion vector for each pixel
+                int sumX = 0, sumY = 0;
+                int count = 0;
+
+                // Iterate over the neighborhood window
+                for (int i = -windowSize; i <= windowSize; i++) {
+                    for (int j = -windowSize; j <= windowSize; j++) {
+                        int nx = x + i;
+                        int ny = y + j;
+
+                        // Check if the neighboring pixel is within the image bounds
+                        if (nx >= 0 && nx < width && ny >= 0 && ny < height) {
+                            // Calculate the difference between pixel values
+                            int dx = pixels[ny * width + nx].r - pixels[y * width + x].r;
+                            int dy = pixels[ny * width + nx].g - pixels[y * width + x].g;
+
+                            // Accumulate the motion vectors
+                            sumX += dx;
+                            sumY += dy;
+                            count++;
+                        }
+                    }
+                }
+
+                // Calculate the average motion vector
+                int avgX = sumX / count;
+                int avgY = sumY / count;
+
+                // Mark the pixel as motion if the motion vector exceeds the threshold
+                if (sqrt(avgX * avgX + avgY * avgY) > threshold) {
+                    motionVectors.pixels[y * width + x].r = 255; // Set red channel to 255
+                    motionVectors.pixels[y * width + x].g = 0;   // Set green channel to 0
+                    motionVectors.pixels[y * width + x].b = 0;   // Set blue channel to 0
+                }
+            }
+        }
+
+        auto end = high_resolution_clock::now();
+        auto duration = duration_cast<milliseconds>(end - start);
+        cout << "---------------  Run Optic Flow: Motion Detection Time: " << duration.count() << "ms" << endl;
+
+        // Save the motion vectors image
+        motionVectors.savePPM("image/out/MotionVectors.ppm");
+    }
+
+    // Optic Flow - Motion Detection MultiThreads
+    void opticFlowThread() {
+        auto start = high_resolution_clock::now(); // Start Timer
+
+        // Define parameters for optic flow calculation
+        int windowSize = 5; // Size of the neighborhood window
+        double threshold = 1.0; // Threshold for motion detection
+
+        // Create a new image to store the motion vectors
+        Image motionVectors(width, height, 255);
+
+        // Define the number of threads to use
+        int num_threads = thread::hardware_concurrency();
+        vector<thread> threads(num_threads);
+
+        // Calculate the number of pixels to process per thread
+        int chunk_size = static_cast<int>(ceil(static_cast<double>(width * height) / num_threads));
+
+        // Function to calculate optic flow for a range of pixels
+        auto calculateOpticFlow = [this, windowSize, threshold, &motionVectors](int start, int end) {
+            // Iterate over the range of pixels
+            for (int index = start; index < end; index++) {
+                int x = index % width;
+                int y = index / width;
+
+                // Calculate motion vector for each pixel
+                int sumX = 0, sumY = 0;
+                int count = 0;
+
+                // Iterate over the neighborhood window
+                for (int i = -windowSize; i <= windowSize; i++) {
+                    for (int j = -windowSize; j <= windowSize; j++) {
+                        int nx = x + i;
+                        int ny = y + j;
+
+                        // Check if the neighboring pixel is within the image bounds
+                        if (nx >= 0 && nx < width && ny >= 0 && ny < height) {
+                            // Calculate the difference between pixel values
+                            int dx = pixels[ny * width + nx].r - pixels[y * width + x].r;
+                            int dy = pixels[ny * width + nx].g - pixels[y * width + x].g;
+
+                            // Accumulate the motion vectors
+                            sumX += dx;
+                            sumY += dy;
+                            count++;
+                        }
+                    }
+                }
+
+                // Calculate the average motion vector
+                int avgX = sumX / count;
+                int avgY = sumY / count;
+
+                // Mark the pixel as motion if the motion vector exceeds the threshold
+                if (sqrt(avgX * avgX + avgY * avgY) > threshold) {
+                    motionVectors.pixels[y * width + x].r = 255; // Set red channel to 255
+                    motionVectors.pixels[y * width + x].g = 0;   // Set green channel to 0
+                    motionVectors.pixels[y * width + x].b = 0;   // Set blue channel to 0
+                }
+            }
+        };
+
+        // Start the threads to calculate optic flow for different ranges of pixels
+        for (int i = 0; i < num_threads; i++) {
+            int start = i * chunk_size;
+            int end = start + chunk_size;
+            if (i == num_threads - 1) {
+                end = width * height; // Last thread handles any remaining pixels
+            }
+            threads[i] = thread(calculateOpticFlow, start, end);
+        }
+
+        // Wait for all threads to finish
+        for (auto& thread : threads) {
+            thread.join();
+        }
+
+        auto end = high_resolution_clock::now();
+        auto duration = duration_cast<milliseconds>(end - start);
+        cout << "---------------  Run Optic Flow Threads: Motion Detection Time: " << duration.count() << "ms" << endl;
+
+        // Save the motion vectors image
+        motionVectors.savePPM("image/out/MotionVectorsThread01.ppm");
+    }
+
 };
 
 int main() {
@@ -148,7 +299,22 @@ int main() {
     cout << endl;
     image.ThreadsInvertColors(); // Parallel Code
     cout << endl;
-    image.savePPM("image/out/Parallel_output01.ppm");
+    
+
+    cout << "----------------------  RunSequential: Optic Flow Motion Detection   ---------------" << endl;
+    cout << endl;
+    image.opticFlow(); // Optic Flow - Motion Detection Secuential
+    cout << endl;
+
+
+    cout << "-------------- Run Parallel: Optic Flow Motion Detection   --------------" << endl;
+    cout << endl;
+    image.opticFlowThread(); // Optic Flow - Motion Detection Multithreads
+    cout << endl;
+
+
+    // Save images
+    image.savePPM("image/out/Parallel_output001.ppm");
     cout << "--------------------------   Save Images PPM     -------------------" << endl;
     cout << endl;
     cout << "                               Code: END                  " << endl;
